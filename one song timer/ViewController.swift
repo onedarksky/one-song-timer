ec0b68d57e754e9fa20d3f88ae5807967c619a89
Ron <onedarksky@icloud.com>
2021-09-10 13:21:36 +0800

    Initial Commit

---
+++ one song timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  one song timer
+//
+//  Created by 江庸冊 on 2021/9/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
